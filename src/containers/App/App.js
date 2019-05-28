458846cbc90b4d426d02c7ffa43d5594d6b64793
Fred Spooner <fred@fredspooner.com>
2019-05-28 18:29:06 +0100

    Some new dirs and moving components around

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Layout from '../../components/Layout/Layout';
 import './App.css';
+import BurgerBuilder from "../BurgerBuilder/BurgerBuilder";
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-
+                <Layout>
+                    <BurgerBuilder />
+                </Layout>
             </div>
         );
     }

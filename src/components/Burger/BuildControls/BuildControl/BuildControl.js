56284e2891cf7d105c78d094ed0da1868a9a9329
Fred Spooner <fred@fredspooner.com>
2019-05-30 14:57:48 +0100

    Applies styling to BuildControl

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ControlCSS from 'BuildControl.module.css';
 
 const buildControl = props => (
-    <div>
-        <div>props.label</div>
-        <button> - </button>
-        <button> + </button>
+    <div className={ControlCSS.BuildControl}>
+        <div className={ControlCSS.Label}>props.label</div>
+        <button className={ControlCSS.Less}> - </button>
+        <button className={ControlCSS.More}> + </button>
     </div>
 );
 

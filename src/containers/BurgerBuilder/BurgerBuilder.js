400bec08421f3f7e1c109d8c1f6379ada69ef95b
Fred Spooner <fred@fredspooner.com>
2019-06-03 13:49:21 +0100

    Small refactors, updates price dynamically, adds reset btn, modal, order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     lettuce: 10,
@@ -10,17 +12,22 @@ const INGREDIENT_PRICES = {
     bacon: 70
 };
 
+const BASE_PRICE = 200;
+const BASE_INGREDIENT_STATE = {
+    lettuce: 0,
+    cheese: 0,
+    bacon: 0,
+    meat: 0
+};
+
 class BurgerBuilder extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            ingredients: {
-                lettuce: 0,
-                cheese: 0,
-                bacon: 0,
-                meat: 0
-            },
-            totalPrice: 400
+            ingredients: BASE_INGREDIENT_STATE,
+            totalPrice: BASE_PRICE,
+            checkoutAllowed: false,
+            showModal: false
         };
     }
 
@@ -38,34 +45,90 @@ class BurgerBuilder extends React.Component {
             totalPrice: newPrice,
             ingredients: updatedIngredients
         });
+        this.updateCheckoutState(updatedIngredients);
     };
 
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
-        const updatedCount = oldCount - 1;
-        if (updatedCount < 0) {
-            return null;
+        if (oldCount <= 0) {
+            return;
         }
+        const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
         updatedIngredients[type] = updatedCount;
-        const priceSubtraction = INGREDIENT_PRICES[type];
+        const priceDeduction = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice - priceSubtraction;
+        const newPrice = oldPrice - priceDeduction;
         this.setState({
             totalPrice: newPrice,
             ingredients: updatedIngredients
         });
+        this.updateCheckoutState(updatedIngredients);
+    };
+
+    updateCheckoutState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(k => {
+                return ingredients[k];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+
+        this.setState({
+            checkoutAllowed: sum > 0
+        });
+    }
+
+    resetIngredientsHandler = () => {
+        const ig = {
+            ...this.state.ingredients
+        };
+
+        Object.keys(ig)
+            .forEach(k => {
+                ig[k] = 0;
+            });
+
+        this.setState({
+            ingredients: ig,
+            totalPrice: BASE_PRICE
+        });
+
+        this.updateCheckoutState(ig)
+    };
+
+    checkoutModalHandler = () => {
+        this.setState({
+            showModal: !this.state.showModal
+        });
     };
 
     render() {
+        const disabled = {
+            ...this.state.ingredients
+        };
+        for (let key in disabled) {
+            disabled[key] = disabled[key] <= 0;
+        }
+
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
+                {/* @TODO: move this to BuildControls: */}
+                <button onClick={this.resetIngredientsHandler}>Reset</button>
                 <BuildControls
                     addClicked={this.addIngredientHandler}
                     removeClicked={this.removeIngredientHandler}
+                    disabledControls={disabled}
+                    currentPrice={this.state.totalPrice}
+                    checkoutAllowed={this.state.checkoutAllowed}
+                    orderButtonClicked={this.checkoutModalHandler}
                 />
             </Aux>
         );

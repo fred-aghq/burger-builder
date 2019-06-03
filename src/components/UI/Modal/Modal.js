6f88573c3e8f7d63e98dacd8c55a8f28730e4a8e
Fred Spooner <fred@fredspooner.com>
2019-06-03 14:22:36 +0100

    Adds transition CSS for modal and dynamically changes class.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ModalCSS from './Modal.module.css';
 
-const modal = props => (
-    <div className={ModalCSS.Modal}>
+const modal = props => {
+    const classes = [ModalCSS.Modal];
+    if (props.show) {
+        classes.push(ModalCSS.Show)
+    }
+    return (<div className={classes.join(' ')}>
         {props.children}
-    </div>
-);
+    </div>);
+};
 
 export default modal;

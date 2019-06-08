1470d68bb093350c86779afc5af677c18c9a665f
Fred Spooner <fred@fredspooner.com>
2019-06-08 19:01:32 +0100

    SideDrawer component + CSS

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import SideDrawerCSS from './SideDrawer.module.css';
+
+const sideDrawer = props => {
+    return (
+        <div className={SideDrawerCSS.SideDrawer}>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+    );
+};
+
+export default sideDrawer;

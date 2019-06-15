fa0f2b094741c759280478259b0a952bb6cb9540
Fred Spooner <fred@fredspooner.com>
2019-06-15 18:02:15 +0100

    Adds Toggle to Toolbar, updates CSS

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ToolbarCSS from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Toggle from '../SideDrawer/Toggle/Toggle';
 
 const toolbar = props => (
     <header className={ToolbarCSS.Toolbar}>
-        <button className={ToolbarCSS.ToggleSideDrawer} onClick={props.toggleSideDrawer}>Menu</button>
+        <Toggle toggle={props.toggleSideDrawer}>Menu</Toggle>
         <div className={[ToolbarCSS.Logo, ToolbarCSS.DesktopOnly].join(' ')}>
             <Logo />
         </div>

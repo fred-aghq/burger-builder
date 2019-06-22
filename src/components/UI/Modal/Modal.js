aeecf35ad5c56f8ac4dd4eda36e1dc16653173b3
Fred Spooner <fred@fredspooner.com>
2019-06-22 17:45:12 +0100

    fixed by converting modal to class and using shouldComponentUpdate

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,24 +3,35 @@ import ModalCSS from './Modal.module.css';
 import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const modal = props => {
-    const classes = [ModalCSS.Modal];
-    if (props.show) {
-        classes.push(ModalCSS.Show)
+class Modal extends React.Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return (nextProps.show !== this.props.show)
+            || (nextProps.children !== this.props.children);
     }
-    return (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div className={classes.join(' ')}>
-            {props.children}
-        </div>
-        </Aux>
-    );
-};
 
-export default React.memo(modal,
-    (prevProps, nextProps) => {
-        return (nextProps.show === prevProps.show)
-            || (nextProps.children === prevProps.children);
+    render() {
+        const classes = [ModalCSS.Modal];
+        if (this.props.show) {
+            classes.push(ModalCSS.Show)
+        }
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.join(' ')}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
     }
-);
+}
+
+export default Modal;
+
+// Leaving this here as a reminder of how to use memo for functional components.
+// I'm not sure why the children check didn't work
+// export default React.memo(modal,
+//     (prevProps, nextProps) => {
+//         return (nextProps.show === prevProps.show)
+//             || (nextProps.children === prevProps.children);
+//     }
+// );

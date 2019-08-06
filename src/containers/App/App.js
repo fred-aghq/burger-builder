cfceef8ce866bffdea4de3f82cd81f1ce6ef2629
Fred Spooner <fred@fredspooner.com>
2019-08-06 17:14:06 +0100

    adds routing and sets up routes for burger builder

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,14 +3,17 @@ import Layout from '../Layout/Layout';
 import './App.css';
 import BurgerBuilder from "../BurgerBuilder/BurgerBuilder";
 import Checkout from '../Checkout/Checkout';
+import {Route, Switch} from 'react-router-dom';
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <Layout>
-                    <BurgerBuilder />
-                    <Checkout />
+                    <Switch>
+                        <Route path="/checkout" component={Checkout} />
+                        <Route path="/" exact component={BurgerBuilder} />
+                    </Switch>
                 </Layout>
             </div>
         );
